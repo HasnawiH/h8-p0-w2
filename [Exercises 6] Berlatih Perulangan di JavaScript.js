ad3efc4dd5d4fb3aa262147b8bcf1a51ec63691b
awinkh <hasnawihaeba@gmail.com>
2019-02-28 15:37:56 +0700

    Menambahkan tugas keenam

--- [Exercises 6] Berlatih Perulangan di JavaScript.js
+++ [Exercises 6] Berlatih Perulangan di JavaScript.js
@@ -33,3 +33,44 @@ for (var b=20; b>=1; b--){
 }
 
 //No 3. Angka Ganjil dan Genap
+
+    // contoh Ganjil-Genap
+
+for (var c=1; c<=100; c++){
+  if (c%2===0){
+    console.log(''+c+' GENAP')
+  }
+  else{
+    console.log(''+c+' GANJIL')
+  }
+}
+
+    //contoh  - untuk pertambahan counter 2
+for (var d=1; d<=100; d+=2){
+  if(d%3===0){
+    console.log(''+d+' KELIPATAN 3')
+  }  
+  else{
+    console.log(d)
+  }
+}
+
+    //contoh - untuk pertambahan counter 5
+for (var e=1; e<=100; e+=5){
+  if(e%6===0){
+    console.log(''+e+' KELIPATAN 6')
+  }  
+  else{
+    console.log(e)
+  }
+}
+
+    //contoh - untuk pertambahan counter 9
+for (var f=1; f<=100; f+=9){
+  if(f%10===0){
+    console.log(''+f+' KELIPATAN 10')
+  }  
+  else{
+    console.log(f)
+  }
+}
